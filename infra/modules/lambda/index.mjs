adf2436397b7cfee938c0ba4f2e13cfd8e2f64ea
davijr <davioliveiraj@hotmail.com>
2024-10-02 01:35:40 -0300

    cpf validation

--- infra/modules/lambda/index.mjs
+++ infra/modules/lambda/index.mjs
@@ -1,18 +1,22 @@
 export const handler = function(event, context) {
   // Retrieve user attribute from event request
-  const userAttributes = event.request.userAttributes;
-  // Add scope to event response
-  event.response = {
-    "claimsAndScopeOverrideDetails": {
-      "idTokenGeneration": {},
-      "accessTokenGeneration": {
-        "claimsToAddOrOverride": {
-          "cpf": userAttributes['custom:cpf']
-        },
-        "scopesToAdd": ["custom:cpf"]
+  const cpf = event.request.userAttributes['custom:cpf'];
+
+  if (cpf) {
+    // Add scope to event response
+    event.response = {
+      "claimsAndScopeOverrideDetails": {
+        "idTokenGeneration": {},
+        "accessTokenGeneration": {
+          "claimsToAddOrOverride": {
+            "cpf": userAttributes['custom:cpf']
+          },
+          "scopesToAdd": ["custom:cpf"]
+        }
       }
-    }
-  };
+    };
+  }
+  
   // Return to Amazon Cognito
   context.done(null, event);
 };
