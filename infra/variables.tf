393ea2e16445be3527c0e5e8ab29ee447a524bf6
Jonas Souza <jonasmzsouza@gmail.com>
2024-10-02 23:48:28 -0300

    config: remove unnecessary variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -61,27 +61,6 @@ variable "api_name" {
   default     = "fastfood-api"
 }
 
-# API Gateway description
-variable "api_description" {
-  description = "API Gateway description"
-  type        = string
-  default     = "API Gateway for FastFood authentication"
-}
-
-# API Gateway part path
-variable "path_part" {
-  description = "Path part for the API Gateway resource"
-  type        = string
-  default     = "auth"
-}
-
-# API Gateway name
-variable "api_name" {
-  description = "API Gateway name"
-  type        = string
-  default     = "fastfood-api"
-}
-
 # Stage name
 variable "stage_name" {
   description = "Stage name"
