e08d32204e904e2617f6bdf0133e329a49711242
Jonas Souza <jonasmzsouza@gmail.com>
2024-10-02 21:37:02 -0300

    fix: declare the variable 'userAttributes' in lambda handler

--- infra/modules/lambda/index.mjs
+++ infra/modules/lambda/index.mjs
@@ -1,18 +1,23 @@
-export const handler = function(event, context) {
+export const handler = function (event, context) {
   // Retrieve user attribute from event request
   const userAttributes = event.request.userAttributes;
-  // Add scope to event response
-  event.response = {
-    "claimsAndScopeOverrideDetails": {
-      "idTokenGeneration": {},
-      "accessTokenGeneration": {
-        "claimsToAddOrOverride": {
-          "cpf": userAttributes['custom:cpf']
+  const cpf = userAttributes["custom:cpf"];
+
+  if (cpf) {
+    // Add scope to event response
+    event.response = {
+      "claimsAndScopeOverrideDetails": {
+        "idTokenGeneration": {},
+        "accessTokenGeneration": {
+          "claimsToAddOrOverride": {
+            "cpf": cpf,
+          },
+          "scopesToAdd": ["custom:cpf:"],
         },
-        "scopesToAdd": ["custom:cpf"]
-      }
-    }
-  };
+      },
+    };
+  }
+
   // Return to Amazon Cognito
   context.done(null, event);
 };
